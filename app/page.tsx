5cabc14a373778c23df49bcb614469c3cfc3f78f
zapboy216 <zapboy216@gmail.com>
2024-08-11 21:24:01 -0400

    create product and transactions components

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
-import  ProductDetails  from '@/components/component/product-Details'
+import  Charts  from '@/components/component/transactions'
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-        <ProductDetails />
+        <Charts />
     </section>
   )
 }
