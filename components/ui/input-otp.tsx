f8d413e1c8cf65a70a9da626355cc4ae7eea5cfc
zapboy216 <zapboy216@gmail.com>
2024-08-15 20:12:09 -0400

    crrect otp error

--- components/ui/input-otp.tsx
+++ components/ui/input-otp.tsx
@@ -11,7 +11,7 @@ const InputOTP = React.forwardRef<
   <OTPInput
     ref={ref}
     containerClassName={cn(
-      "flex items-center gap-2 has-[:disabled]:opacity-50",
+      "has-[:disabled]:opacity-50 flex items-center gap-2",
       containerClassName
     )}
     className={cn("disabled:cursor-not-allowed", className)}
@@ -48,7 +48,7 @@ const InputOTPSlot = React.forwardRef<
       {char}
       {hasFakeCaret && (
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-          <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
+          <div className="animate-caret-blink h-4 w-px bg-foreground duration-1000" />
         </div>
       )}
     </div>
